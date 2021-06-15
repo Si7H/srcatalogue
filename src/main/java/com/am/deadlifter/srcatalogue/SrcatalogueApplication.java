8d747a64e0b0fba1e275fada46f7cb9921b52c4c
Si7H <arkadiusz.mecka@gmail.com>
2021-06-15 19:40:15 +0200

    Initial project schema

---
+++ src/main/java/com/am/deadlifter/srcatalogue/SrcatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.am.deadlifter.srcatalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrcatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrcatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/am/deadlifter/srcatalogue/SrcatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.am.deadlifter.srcatalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrcatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
